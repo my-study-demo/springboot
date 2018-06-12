f85c62f765caa3214f8467dcb929225c8c749c36
yangqinghua <yangqinghua39@163.com>
2018-06-12 23:34:42 +0800

    rabbitmq consumer node2

---
+++ samples/rabbitmq-demo/consumer-node2/src/main/java/com/kely/rabbitmq/demo/consumer/RabbitmqConsumerNode2Application.java
@@ -0,0 +1,22 @@
+package com.kely.rabbitmq.demo.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsumerNode2Application {
+    static Logger logger = LoggerFactory.getLogger(RabbitmqConsumerNode2Application.class);
+
+    /**
+     * rabbitmq消费者启动入口
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqConsumerNode2Application.class, args);
+
+        logger.info("【【【【【消息队列【节点2】-消息消费者启动成功.】】】】】");
+    }
+}
--- samples/rabbitmq-demo/provider/src/test/java/com/kely/rabbitmq/demo/provider/UserTester.java
+++ samples/rabbitmq-demo/provider/src/test/java/com/kely/rabbitmq/demo/provider/UserTester.java
@@ -50,4 +50,54 @@ public class UserTester {
                 .andDo(MockMvcResultHandlers.log())
                 .andReturn();
     }
+
+    /**
+     * 测试用户批量添加
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testBatchUserAdd() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            //创建用户注册线程
+            Thread thread = new Thread(new BatchRabbitTester(i));
+            //启动线程
+            thread.start();
+        }
+        //等待线程执行完成
+        Thread.sleep(2000);
+    }
+
+    /**
+     * 批量添加用户线程测试类
+     * run方法发送用户注册请求
+     */
+    class BatchRabbitTester implements Runnable {
+        private int index;
+
+        public BatchRabbitTester() {
+        }
+
+        public BatchRabbitTester(int index) {
+            this.index = index;
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                mockMvc.perform(MockMvcRequestBuilders.post("/user/save")
+                        .param("userName", "yuqiyu" + index)
+                        .param("name", "恒宇少年" + index)
+                        .param("age", "23")
+                )
+                        .andDo(MockMvcResultHandlers.log())
+                        .andReturn();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
